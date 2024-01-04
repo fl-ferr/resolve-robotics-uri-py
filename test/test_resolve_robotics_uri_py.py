3dafe71a9370c51b4ed00f1feb6036fd6af8bfad
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2024-01-04 17:06:52 +0100

    Cleanup

--- test/test_resolve_robotics_uri_py.py
+++
@@ -1,6 +0,0 @@
-import resolve_robotics_uri_py
-import pytest
-
-def test_non_existing_file():
-    with pytest.raises(FileNotFoundError):
-        resolve_robotics_uri_py.resolve_robotics_uri("package://this/package/and/file/does/not.exist")
